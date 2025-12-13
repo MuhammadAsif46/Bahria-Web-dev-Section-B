e458c6253e1919fd347e49e61447d14f9fbb4301
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-13 15:52:52 +0500

    sat class update code

---
+++ JAVASCRIPT/05. if esle, else-if & testing sets of conditions/app.js
@@ -0,0 +1,57 @@
+// Chapter: 12 - if...else and else if statements
+
+// var chair = prompt("Enter value ???")
+
+// if(chair == "big"){
+//     console.log("bari chair Lekay ajao..");
+// } else if (chair == "dice") {
+//     console.log("dice Lekay ajao..");
+// } else if (chair == "duster") {
+//     console.log("Duster Lekay ajao..");
+// } else {
+//     console.log("Khali Hath ajao..");
+// }
+
+
+// Chapter: 13 - Testing sets of conditions
+
+// &&  right and left side both are true
+// ||  right or left any one side is true
+
+// var language = prompt("Enter your language?")
+// var loc = prompt("Enter your location?")
+// var exp = prompt("Enter your experience?")
+
+// if(language === "html" && loc === "karachi" && exp === "1 year"){
+//     console.log(" You Are Hired!");
+// }else {
+//     console.log("You are not hired");
+// }
+
+// ------------- && ----------------
+
+// true && true
+// true
+// true && false
+// false
+// false && true
+// false
+// false && false
+// false
+
+// ------------- || ---------------
+
+// true || true
+// true
+// true || false
+// true
+// false || true
+// true
+// false || false
+// false
+
+// if(language === "html" || loc === "karachi" || exp === "1 year"){
+//     console.log(" You Are Hired!");
+// }else {
+//     console.log("You are not hired");
+// }
