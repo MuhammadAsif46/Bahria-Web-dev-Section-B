d3e65552b327b10b4c4de3d0a1b190b856445b9b
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-20 18:55:33 +0500

    update today code

---
+++ JAVASCRIPT/06. revision-previous-all-chapters-01-14/app.js
@@ -0,0 +1,101 @@
+// ========================= //
+
+// console.log("hello");
+
+// alert("Hello")
+
+// Data type:
+'hbgfoafbdbdfj' //string
+3423654         // number
+true/false      // boolean
+
+// var username = "Smith"
+// alert(username)
+// username = "Clerk"
+// alert(username)
+
+
+
+// var age = 14
+// console.log(age);
+// age = 15 + 6
+// console.log(age);
+
+
+// var informationTechnology = "IT"
+
+// var a = 14
+// var b = 4
+// var c = a % b
+// console.log(c);
+
+
+// var a = 5
+// // a = a + 2
+// a++     // post increment
+// a--     // post decrement
+// --a     // pre decrement
+// ++a     // pre increment
+// console.log(a);
+
+
+// var username = "Smith"
+// var fatherName = "Clerk"
+// console.log("Welcome to our college " + username);
+
+// console.log(username + " " + fatherName);
+
+// var value = +prompt("Enter your age", "+92")
+// console.log(value);
+
+// var a = 56
+
+// if (a == 5){
+//     console.log("running..")
+// }
+
+// var a = 6
+
+// if(a != 6){
+//     console.log("Running...");
+// }
+
+
+// var a = 6
+
+// var a = +prompt("Enter a number")
+// // var b = +prompt("Enter a number")
+
+// if(a == 1000 || a < 1200){
+//     console.log("1000 between 1200...");
+// } else {
+//     console.log("Not Running...");
+// }
+
+
+var a = 2
+var b = 5
+var c = 6
+var d = 2
+var e = 5
+var f = 1
+
+if(a == d || e == c){
+    if(b == 4){
+        console.log("1st answer is correct");
+    } else {
+        console.log("1st answer is in correct");
+    }
+}else {
+    console.log("answer is incorrect");
+}
+
+
+
+// Assignment - 01
+
+// Chapter : 01 (Q-1)
+// alert("Weclcome")
+
+
+// Chapter : 01 (Q-2)
