d428fec8b445a19b24dbf206f0e6d35f92e42481
muhammadasif46 <asifahmed32c@gmail.com>
2025-12-21 19:14:35 +0500

    update code class today

---
+++ JAVASCRIPT/07. arrays/app.js
@@ -0,0 +1,56 @@
+// Arrays:
+
+// push()       -> add value last index
+// pop()        -> remove value last index
+// shift()      -> remove value first index
+// unshift()    -> add value first index
+// splice()     -> add/delete/update 
+// slice()      -> copy values
+
+// var std1 = "Jhon"
+// var std2 = "Hamza"
+// var std3 = "Find me"
+
+// var cities = ["karachi", "lahore", "pindi"];
+// var mixedArr = [1, true, "hamza", false, 10]
+
+// console.log(cities[2]);
+// console.log(mixedArr[2]);
+
+
+// var cities = ["karachi", "lahore", "pindi"];
+
+// var cities = ["Karachi","Karachi","Karachi","Karachi","Karachi","Karachi","Karachi","Karachi","Karachi","Karachi"];
+
+// cities[0] = "Karachi"
+// cities[1] = "multan"
+// cities[2] = "lahore"
+// cities[10] = "lahore"
+
+// var cities = ["karachi", "lahore", "pindi"];
+// cities.push("islamabad", "multan")
+// cities.pop()
+// cities.pop()
+// console.log(cities);
+
+
+// var cities = ["karachi", "lahore", "pindi"];
+
+// cities.shift()
+// cities.unshift("multan","islamabad")
+// console.log(cities);
+
+
+// var cities = ["karachi", "lahore","multan"];
+
+// slice(index, length)
+// var copyArr = cities.slice(2, 4)
+// console.log(copyArr);
+// splice(index, deleted-count, other-values)
+// console.log(cities);
+
+
+// var cities = ["karachi", "lahore","multan", "pindi"];
+// // splice(index, deleted-count, other-values)
+// cities.splice(1, 0, "isl","pes","quetta")
+// console.log(cities);
