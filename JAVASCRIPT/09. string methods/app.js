3bd986f0b32d175026c48b313d0abb588c1b5aff
muhammadasif46 <asifahmed32c@gmail.com>
2026-01-04 18:44:40 +0500

    update today class code

---
+++ JAVASCRIPT/09. string methods/app.js
@@ -0,0 +1,58 @@
+// Array methods:
+
+// sort()       -> array elements ko sort karta hai
+// reverse()    -> array ko string me convert karta hai
+// split()      -> string ko array me convert karta hai
+// join()       -> array order reverse karta hai
+
+
+// Sort:
+// var numbers = [1,4,7,9,3, 2000,5,2, 10, 100]
+// numbers.sort()
+// console.log(numbers);
+
+// var alpa = ['d', 'z', 'dd', 'y', 'a', 'o']
+// alpa.sort()
+// console.log(alpa);
+
+
+// Reverse:
+// var cities = ['khi', 'lhr', 'isl', 'pindi']
+// cities.reverse()
+// console.log(cities);
+
+
+// Split:
+// var value = "hello"
+// console.log(value.split(""));
+
+// Join:
+// var value = ["This", "is", "an", "Apple"]
+// console.log(value.join("/"));
+
+
+// String Methods:
+
+// toLowerCase() - done
+// toUpperCase() - done
+// Replace() - done
+// ReplaceAll() - done
+// slice() - done
+// length - done
+// IndexOf()
+// lastIndexOf()
+// chartAt()
+
+// var data = "COLLEGE"
+// console.log(data.toLowerCase());
+// var data1 = "college"
+// console.log(data1.toUpperCase());
+
+// var text = "This is and is add that some time?"
+// console.log(text.replace("and", "or"));
+// console.log(text.replaceAll("is", "am"));
+
+// var text = "hello world"
+// var extractPart = text.slice(7, 9)
+// console.log(text.length);
+// console.log(extractPart);
