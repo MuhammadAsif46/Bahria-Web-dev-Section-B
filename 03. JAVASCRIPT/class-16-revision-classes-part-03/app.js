c0e3aa4cac35b332fa2b250a9108d12c86a7351a
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-19 16:32:13 +0500

    JS revision class part-03

---
+++ 03. JAVASCRIPT/class-16-revision-classes-part-03/app.js
@@ -0,0 +1,71 @@
+// ------ JS CHAPTER REVISION 13 TO 20 ------ //
+
+// var weight = 500
+// var time = 7
+
+// if (weight > 300 || time < 6) {
+//   alert("Come to our tryout!");
+// } else {
+//   alert("Come to our cookout!");
+// }
+
+// var weight = 500;
+// var time = 7;
+
+// if (weight > 300) {
+//   if (time < 6) {
+//     alert("Come to our time!");
+//   } else {
+//     alert("Come to our weight!");
+//   }
+// } else {
+//   alert("Come to our cookout!");
+// }
+
+// var cities = ["Khi", "Lhr", "Isl", "Pindi"]
+// // console.log(cities[2]);
+// console.log(cities);
+// cities.push("Quetta")
+// cities.unshift("Dha")
+// cities.pop()
+// cities.shift()
+// console.log(cities);
+
+// // var favCities = cities.slice(2, 4)
+// // console.log(favCities);
+// // cities.splice(2, 1)
+// cities.splice(2, 0, "Quetta")
+// console.log(cities);
+
+// var table = prompt("Enter your fav number")
+
+// for(var i = 1; i <= 10; i++){
+//   console.log(table + " x " + i + " = " + (table * i) + "\n");
+// }
+
+// var cities = ["Khi", "Lhr", "Isl", "Pindi"]
+// var flag = false
+
+// for (let i = 0; i < cities.length; i++) {
+//   // console.log(cities[i]);
+//   if(cities[i] === "Quetta"){
+//     flag = true;
+//     break;
+//   }
+// }
+// if(flag){
+//   console.log("welcome our city");
+// } else {
+//   console.log("Not Allowed");
+// }
+
+// for (let i = 1; i <= 4; i++) {
+//   for (let j = 1; j <= 2; j++) {  
+//     console.log(i, j);
+//   }
+// }
+
+// 1, 1
+// 1, 2
+// 2, 1
+// 2, 2
