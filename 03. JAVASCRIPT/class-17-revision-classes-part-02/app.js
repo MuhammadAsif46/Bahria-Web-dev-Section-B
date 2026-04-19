ef354daa8aeee2f6df3598f0896bc2349a6b58b1
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-19 12:42:04 +0500

    JS revision class part-02

---
+++ 03. JAVASCRIPT/class-17-revision-classes-part-02/app.js
@@ -0,0 +1,36 @@
+// ------ JS CHAPTER REVISION 7 TO 12 ------ //
+
+// var firstName = "Hamza"
+// var lastName = "Ali"
+
+// console.log("Welcome to " + firstName + " " + lastName);
+
+// var user = prompt("Enter your Name")
+// var user = +prompt("Enter your Age")
+// console.log(user + 3);
+
+// var num = 3
+
+// if (num < 2) {
+//   console.log("Hello");
+// }
+// console.log("World");
+
+// ==
+// !=
+// ===
+// !==
+// >
+// <
+// >=
+// <=
+
+var num = 4;
+
+if (num == 5) {
+  console.log("Your First Condition is True");
+} else if (num <= 4) {
+  console.log("Your Second Condition is True");
+} else {
+  console.log("Your Condition is False");
+}
