645c81c644ea080ee34c27356795563448ca1a66
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-27 12:15:03 +0500

    Js revision class part-05

---
+++ 03. JAVASCRIPT/class-18-revision-classes-part-05/app.js
@@ -0,0 +1,54 @@
+// ------ JS CHAPTER REVISION 31 TO 37 ------ //
+
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+// var date = new Date()
+// console.log(date);
+// console.log(days[date.getDay()]);
+
+// var eid_ul_adha = new Date("May 27, 2026").getTime()
+// var getTime = date.getTime()
+// console.log(getTime);
+// console.log(eid_ul_adha);
+// console.log(eid_ul_adha - getTime);
+// var result = eid_ul_adha - getTime
+// var finalAnswer = result / (1000 * 60 * 60 * 24)
+// console.log(Math.floor(finalAnswer) + " Days are left in eid-ul-adha");
+
+
+// function greet(a, z, kuchbhi){ // parameters
+//     console.log("Welcome " + a);
+//     console.log("Welcome " + z);
+//     console.log("Welcome " + kuchbhi);
+// }
+
+// greet("Hamza", 440, false) // argument
+// greet("Ali", 440, false) // argument
+// greet("Ahmed", 440, false) // argument
+// greet("Hamza", 440, false) // argument
+
+
+function sum(a, b){
+    console.log(a + b);
+}
+
+sum(3, 10)
+sum(27, 15)
+
+
+function foo(){
+    var a = 123
+    return a
+}
+// console.log(a);
+
+var res = foo()
+console.log(res);
+
+// prompt()
+// function alert(a){
+//     console.log(a);
+    
+// }
+
+// alert("Hello")
