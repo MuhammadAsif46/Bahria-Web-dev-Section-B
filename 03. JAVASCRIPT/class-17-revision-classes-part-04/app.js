92802467bac9962b31e1833843cb0170676c253c
muhammadasif46 <asifahmed32c@gmail.com>
2026-04-27 12:14:36 +0500

    Js revision class part-04

---
+++ 03. JAVASCRIPT/class-17-revision-classes-part-04/app.js
@@ -0,0 +1,26 @@
+// ------ JS CHAPTER REVISION 21 TO 30 ------ // 
+
+
+// var userName = prompt()
+
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+// console.log(userName.length);
+// var firstChar = userName.slice(0,1).toUpperCase()
+// var lastChar = userName.slice(1).toLowerCase()
+// console.log(firstChar + lastChar);
+
+// var text = "But JavaScript has a more efficient way more to accomplish this"
+// console.log(text.indexOf("more"));
+// console.log(text.lastIndexOf("more"));
+
+// console.log(text.charAt(21));
+// console.log(text.charAt(38));
+
+// console.log(text.replace("more", "many"));
+// console.log(text.replaceAll("more", "many"));
+
+
+var num = 123
+console.log(typeof num, num);
+console.log(typeof num.toString());
