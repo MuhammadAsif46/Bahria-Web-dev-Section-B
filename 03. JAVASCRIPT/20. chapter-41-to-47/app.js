52ee47f6b87107b537071334197415c72d455c5a
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:35:49 +0500

    JS class cover 7 chapters

---
+++ 03. JAVASCRIPT/20. chapter-41-to-47/app.js
@@ -0,0 +1,34 @@
+// ------ JS CHAPTER 41 TO 47 ------ //
+
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// var i = 0;
+// while(i <= 3){
+//   console.log(i);
+//   i++
+// }
+
+// var i = 0;
+// do {
+//   console.log(i);
+//   i++
+// } while(i > 4);
+
+// var a = "abc"
+
+/* if(true){
+  console.log("running..");
+  
+} */
+// var
+
+function sum(a, z){
+  alert(a + z);
+  
+}
+function mul(a, z){
+  alert(a * z);
+  
+}
