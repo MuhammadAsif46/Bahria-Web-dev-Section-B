242f70268f422e1f1c6505d4bcb0a4ddcb697b04
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:37:33 +0500

    JS class cover 5 chapters

---
+++ 03. JAVASCRIPT/23. chapter-56-to-60/app.js
@@ -0,0 +1,25 @@
+// ------ JS CHAPTER 56 TO 60 ------ //
+
+// var para = document.getElementById("para");
+// console.log(para);
+
+// var heading = document.getElementsByTagName("h1");
+
+// function change(){
+//   var para = document.getElementsByTagName("p");
+//   para[0].style.color = "red";
+//   para[1].innerText = "Pakistan"
+//   para[2].innerHTML = "<h2>Username</h2>"
+// }
+ 
+// var main = document.getElementById("main")
+// console.log(main);
+
+// var para = main.getElementsByTagName("p")
+// console.log(para);
+
+
+
+// console.log(document.childNodes[1].childNodes[1].childNodes[1]);
+var heading = document.childNodes[1].childNodes[1].childNodes[1];
+heading.style.backgroundColor = "red"
