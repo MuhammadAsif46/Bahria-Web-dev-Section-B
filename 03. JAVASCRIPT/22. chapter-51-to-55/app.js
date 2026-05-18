ea5b3870e03cb992bde021b97a25b1517d6fb20f
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:36:59 +0500

    JS class cover 5 chapters

---
+++ 03. JAVASCRIPT/22. chapter-51-to-55/app.js
@@ -0,0 +1,37 @@
+// ------ JS CHAPTER REVISION 51 TO 55 ------ //
+
+// function submit(){
+//   var name = document.getElementById("name")
+//   var email = document.getElementById("email")
+//   var course = document.getElementById("course")
+//   alert(name.value + "\n" + email.value + "\n" + course.value)
+// }
+
+// function clearAll(){
+//   var name = document.getElementById("name").value = ""
+//   var email = document.getElementById("email").value = " "
+//   var course = document.getElementById("course").value = " "
+// }
+
+// document.getElementById("para").innerHTML = "<h1>hello</h1>"
+// document.getElementById("para").innerText = "hello"
+// var main = document.getElementById("main")
+// main.innerHTML = "<p>Hello world</p>"
+// console.log(main.innerHTML);
+
+// function seemore() {
+//   var main = document.getElementById("main");
+//   var btntext = document.getElementById("btn");
+//   main.innerText =
+//     "Pakistan,[f] officially the Islamic Republic of Pakistan,[g] is a country in South Asia. It is the fifth-most populous country, with a population of over 241.5 million,[d] having the second-largest Muslim population as of 2023. Islamabad is the nation's capital, while Karachi is its largest city and financial centre. Pakistan is the 33rd-largest country by area. Bounded by the Arabian Sea on the south, the Gulf of Oman on the southwest, and the Sir Creek on the southeast, it shares land borders with India to the east; Afghanistan to the west; Iran to the southwest; and China to the northeast. It shares a maritime border with Oman in the Gulf of Oman, and is separated from Tajikistan in the northwest by Afghanistan's narrow Wakhan Corridor.";
+//   btntext.innerText = "See Less";
+// }
+
+function abc(){
+    var main = document.getElementById("main");
+    // console.log(main.className += " hidden");
+    main.style.backgroundColor = "red"
+    main.style.color = "white"
+    main.style.padding = "20px"
+    
+}
