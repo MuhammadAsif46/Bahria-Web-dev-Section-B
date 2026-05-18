6cbbe87749724a70cd1d04602fd6e079870f1164
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:36:27 +0500

    JS class cover 3 chapters

---
+++ 03. JAVASCRIPT/21. chapter-48-to-50/app.js
@@ -0,0 +1,14 @@
+// ------ JS CHAPTER 48 TO 50 ------ //
+function submit() {
+  var email = document.getElementById("email");
+  console.log(email.value  = "Pakistan"); 
+  // console.log(email.value);
+  // if(email.value === ""){
+  //   alert("Please Enter Email address")
+  // }
+}
+
+// var a = "abc"
+// a = "xyz"
+// console.log(a);
+
