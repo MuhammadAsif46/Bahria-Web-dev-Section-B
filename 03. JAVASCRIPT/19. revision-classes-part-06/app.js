28b63e131f5b9f54ffc1f8ccaa48ad61dade8c3f
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-18 11:34:32 +0500

    JS revision class part-06

---
+++ 03. JAVASCRIPT/19. revision-classes-part-06/app.js
@@ -0,0 +1,27 @@
+// ------ JS CHAPTER REVISION 38 TO 40 ------ //
+
+// var a = "haris" // global var
+// function abc(){
+//     var b = "Find me" // local var
+//     console.log(b);
+// }
+
+// console.log(a);
+// abc()
+// console.log(b);
+
+var day = "Wed";
+var month = "JAN";
+switch (day) {
+  case "Sat":
+    console.log("Today is Saturday");
+    break;
+  case "Sun":
+    console.log("Today is Sunday");
+    break;
+  case "Fri":
+    console.log("Today is Friday");
+    break;
+  default:
+    alert("Today is Another day");
+}
