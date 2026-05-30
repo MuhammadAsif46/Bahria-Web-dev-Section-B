ecc60288bebe6ee3a8bde1de2d4dd765f0255fa4
muhammadasif46 <asifahmed32c@gmail.com>
2026-05-30 14:46:14 +0500

    JS class cover 2 chapters

---
+++ 03. JAVASCRIPT/26. chapter-69-and-70/app.js
@@ -0,0 +1,50 @@
+// ------ JS CHAPTER 69 and 70 ------ //
+
+ 
+// var std1Name = "ahmed"
+// var std1Fname = "Ali"
+// var std1age = 19
+
+// var std2Name = "" 
+// var std2fname =  ""
+// var std2age = 12
+
+// Create object
+// add object data
+// update object data 
+// get object  data
+
+// var student1 = {
+//     name: "Hamza",
+//     fatherName: "Qari sahab",
+//     rollNo: 420,
+//     phone: "090078601"     
+// }
+// student1.email = "hamza@gmail.com"
+// student1.phone = 9999999
+
+// delete student1.fatherName
+
+// console.log(student1);
+// console.log(student1.phone);
+// console.log(student1.rollNo);
+// console.log(student1["email"]);
+
+
+var car = {
+    name: "Supra",
+    model: 2020,
+    tyre: 4,
+    color: ["white", "black", "gray"],
+    price: 8_00_00_000 ,
+    isAuto: true,
+    // door: {
+
+    // }
+} 
+
+// console.log(car.color[1]);
+// console.log(car.email);
+console.log("email" in car);
+console.log("model" in car);
+
