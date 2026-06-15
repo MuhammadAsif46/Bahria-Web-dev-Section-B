1adb18033f8add1030192941b657cb76ead3cd74
muhammadasif46 <asifahmed32c@gmail.com>
2026-06-16 00:01:12 +0500

    JS calculator app

--- 03. JAVASCRIPT/27. Calculator-app/app.js
+++ 03. JAVASCRIPT/27. Calculator-app/app.js
@@ -1,24 +1,129 @@
-var inputValue = document.getElementById("displayValue")
-// console.log(inputValue.value);
+ 
+
+// var displayValue = document.getElementById("displayValue");
+
+// var firstNumber = "";
+// var selectedOperator = "";
+// var secondNumber = "";
+// var isOperatorClicked = false;
+
+// function number(val) {
+//   if (isOperatorClicked === false) {
+//     firstNumber += val;
+//     displayValue.value = firstNumber;
+//   } else {
+//     secondNumber += val;
+//     displayValue.value = firstNumber + selectedOperator + secondNumber;
+//   }
+// }
+
+// function operator(op) {
+//   if (firstNumber === "") {
+//     return;
+//   }
+
+//   if (selectedOperator !== "" && secondNumber === "") {
+//     selectedOperator = op;
+//     displayValue.value = firstNumber + selectedOperator;
+//     return;
+//   }
+
+//   selectedOperator = op;
+//   isOperatorClicked = true;
+//   displayValue.value = firstNumber + selectedOperator;
+// }
+
+// function decimalPoint() {
+//   if (isOperatorClicked === false) {
+//     if (!firstNumber.includes(".")) {
+//       firstNumber += ".";
+//       displayValue.value = firstNumber;
+//     }
+//   } else {
+//     if (!secondNumber.includes(".")) {
+//       secondNumber += ".";
+//       displayValue.value = firstNumber + selectedOperator + secondNumber;
+//     }
+//   }
+// }
+
+// function calculate() {
+//   if (firstNumber === "" || selectedOperator === "" || secondNumber === "") {
+//     return ;
+//   }
+
+//   var num1 = Number(firstNumber);
+//   var num2 = Number(secondNumber);
+//   var result;
+
+//   switch (selectedOperator) {
+//     case "+":
+//       result = num1 + num2;
+//       break;
+
+//     case "-":
+//       result = num1 - num2;
+//       break;
+
+//     case "*":
+//       result = num1 * num2;
+//       break;
+
+//     case "/":
+//      if(num2 === 0) {
+//           result = " "
+//           alert("0 is Not Allowed")
+//           break;
+//      } else {
+//           result = num1 / num2;
+//           break;
+//      }
+
+//     case "%":
+//       result = num1 % num2;
+//       break;
+//   }
+
+//   displayValue.value = result;
+
+//   firstNumber = result.toString();
+//   secondNumber = "";
+//   selectedOperator = "";
+//   isOperatorClicked = false;
+// }
+
+// function clearBtn() {
+//   displayValue.value = "";
+//   firstNumber = "";
+//   secondNumber = "";
+//   selectedOperator = "";
+//   isOperatorClicked = false;
+// }
+
+// function deleteBtn() {
+//   if (secondNumber !== "") {
+//     secondNumber = secondNumber.slice(0, -1);
+//   } else if (selectedOperator !== "") {
+//     selectedOperator = "";
+//     isOperatorClicked = false;
+//   } else if (firstNumber !== "") {
+//     firstNumber = firstNumber.slice(0, -1);
+//   }
+
+//   displayValue.value = firstNumber + selectedOperator + secondNumber;
+// //   displayValue.value = displayValue.value.slice(0, -1)
+// }
+
+// var characterTop;
+
+// document.addEventListener('keydown', function (event) {
+//   console.log(`Key pressed: ${event.key}`);
+
+//   if(event.key === "Shift"){
+//      alert("Shift key is pressed")
+//      characterTop = 10px
+//   }
+// });
 
 
-function number (val){
-    // console.log(val);
-     inputValue.value += val 
-    
-    
-}
-
-function clearAll(){
-     inputValue.value = ""
-}
-
-function delBtn(){
-     inputValue.value =  inputValue.value.slice(0, -1)
-}
-
-function operator (value){
-    // console.log(value);
-    inputValue.value += value
-    
-}
+ 
