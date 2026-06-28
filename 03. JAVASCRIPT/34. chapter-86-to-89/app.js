0f8c6e3acd54a2778e82b73234eea1be6e470734
Muhammad Asif <asifahmed32c@gmail.com>
2026-06-28 21:50:01 +0500

    javascript book end

---
+++ 03. JAVASCRIPT/34. chapter-86-to-89/app.js
@@ -0,0 +1,12 @@
+// ------ JS CHAPTER 86 to 89 ------ //
+
+console.log("Pakistan");
+
+try { 
+  aler("Karachi")
+} catch(err){
+  console.log("aler spelling is incorrect");
+}
+
+console.log("Zindabad");
+  
