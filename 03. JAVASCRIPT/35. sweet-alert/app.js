2bddeedc0980fefff25623691c78f1afa93467ef
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-12 16:14:29 +0500

    js class sweet alert

---
+++ 03. JAVASCRIPT/35. sweet-alert/app.js
@@ -0,0 +1,15 @@
+// alert("Hello world")
+
+function showAlert() {
+  Swal.fire({
+    // title: "Behtreen",
+    // text: "This is an Apple",
+    // icon: "question"
+    imageUrl: "https://picsum.photos/200/300",
+    imageHeight: 300,
+    imageWidth: 300,
+    imageAlt: "A tall image"
+  });
+}
+
+
