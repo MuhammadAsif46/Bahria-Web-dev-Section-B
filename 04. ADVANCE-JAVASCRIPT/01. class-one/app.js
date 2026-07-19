b72a09ce53225e01686ee01ff23913303f2c0b32
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-20 00:17:24 +0500

    Advance JS topics part 01

---
+++ 04. ADVANCE-JAVASCRIPT/01. class-one/app.js
@@ -0,0 +1,80 @@
+// ====== ADVANCE JAVASCRIPT PART 01 ====== //
+
+// var
+// let
+// const
+
+// var a = 5
+
+// function foo(){
+//     let xyz = "hello"
+// }
+// console.log(xyz);
+
+
+// if (true) {
+//     let xyz = "hello"
+//     console.log(xyz);
+// }
+// console.log(xyz);
+
+
+// const xyz = "Jhon"
+
+// xyz = "Clerk"
+
+// console.log(xyz);
+
+
+// var xyz = "hello"
+
+// var xyz = "world"
+
+// console.log(xyz);
+
+
+// const xyz = "hello"
+
+// const xyz = "world"
+
+// console.log(xyz);
+
+// let firstname = "Ali"
+// let lastname = "Ahmed"
+// let city = "Islamabad"
+// // let fullname = "Welcome, " + firstname + " " + lastname + " From " + city
+// let fullname = `Welcome, ${firstname} ${lastname} From ${city}`
+// console.log(fullname);
+
+
+// let age = 18
+
+// if(age >= 18){
+//     console.log("you are eligiable");
+// } else {
+//     console.log("you are not eligiable");
+// }
+
+// let age = 17; age >= 18 ? console.log("you are eligiable") : console.log("you are not eligiable");
+// let age = 17; 
+// age >= 18 ? "eligiable" : "not eligiable"
+
+// condition ? true : false
+
+let car = {
+    brand: "Civic",
+    company: "Honda",
+    price: 4_00_00_00,
+    otherDetail : {
+        tyre: 3
+    }
+}
+
+console.log("1");
+
+
+// console.log(car.model);
+console.log(car?.otherDetail?.tyre);
+
+
+console.log("2");
