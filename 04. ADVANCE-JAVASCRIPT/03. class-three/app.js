5e31808eb47b8dc3f4666b2ffe73f6846ec2bb14
muhammadasif46 <asifahmed32c@gmail.com>
2026-07-25 14:40:33 +0500

    Advance JS Topics Part 03

---
+++ 04. ADVANCE-JAVASCRIPT/03. class-three/app.js
@@ -0,0 +1,119 @@
+
+
+// ------ Destructuring ----- //
+
+// let studentInformation = {
+//     name: "Find me",
+//     age: 17,
+//     grade: '11th'
+// }
+
+// let { age, name, grade } = studentInformation
+
+
+// console.log(studentInformation.name);
+// console.log(studentInformation.grade);
+// console.log(studentInformation.age);
+
+// console.log(age);
+// console.log(name);
+// console.log(grade);
+
+// let countries = ["Pakistan", "China", "Nepal"]
+
+// let [a, c, b] = countries
+// let [hello, user, world] = countries
+// console.log(hello, world, user);
+
+
+// -------- Enhanced object literals ---- //
+
+// let username = "Raza"
+// let email = "raza@gmail.com"
+
+// let obj = {
+//     name: username,
+//     email,
+// }
+
+// console.log(obj);
+
+
+// ------ Iterators & For..of  ------- //
+
+
+let countries = ["Pakistan", "China", "Nepal"]
+
+// for(let i = 0; i < countries.length; i++){
+//     console.log(countries[i]);
+// }
+
+// for values:
+// for (let country of countries) {
+//     console.log(country);
+// }
+
+// for index no:
+// for (let country in countries) {
+//     console.log(country);
+// }
+
+
+// ------ Arrow functions ---- //
+
+// function foo(){
+//     console.log("test");
+// }
+
+// foo()
+
+// let greet = function(){
+//     console.log("Welcome");
+// }
+
+// greet()
+
+
+
+// function foo(a){
+//     return a + 10;
+// }
+
+// let res = foo(10)
+// console.log(res);
+
+
+// arrow function 
+
+// let greet = num => num + 10;   
+
+
+// let result = greet(10)
+// console.log(result);
+
+
+// let sum = (a, b) => a + b
+// let mul = (a, b) => a * b
+// let divide = (a, b) => a / b
+// let sub = (a, b) => a - b
+
+
+// ---- Classes ----- //
+
+// function person(){}
+// function Person(){}
+
+// class Student {
+//     constructor(name, fname , age , email) {
+//         this.name = name,
+//         this.fname = fname,
+//         this.age = age,
+//         this.email = email
+//     }
+// }
+
+// let std1 = new Student("Jhon", "Elia", 74, "jhon@gmail.com")
+// let std2 = new Student("Find", "me", 17, "findme@gmail.com")
+// console.log(std1);
+// console.log(std2);
+
