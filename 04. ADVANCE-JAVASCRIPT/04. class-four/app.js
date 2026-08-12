aa65493df1c6c26e492b9e1aadaacf537ab3a503
muhammadasif46 <asifahmed32c@gmail.com>
2026-08-12 10:24:26 +0500

    Advance JS Topics part 4

---
+++ 04. ADVANCE-JAVASCRIPT/04. class-four/app.js
@@ -0,0 +1,126 @@
+// -------- Array Methods ------ //
+
+// forEach() - 
+// map() -
+// filter() -
+// find() -
+// reduce() -
+// some() -
+// every() -
+// includes() - 
+// join() -
+// split() -
+// sort() - 
+// concat() - 
+// reverse() - 
+
+let fruits = ["Apple", "Mango", "Orange", "Kiwi"]
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// let res = fruits.f20orEach(function(v, i){
+//     console.log(v, i);
+// })
+
+// console.log(res);
+
+
+// let result = fruits.map(function(v, i){
+//     console.log(v, i);
+//     return v + " Bahria"
+// })
+
+// console.log(result);
+
+
+// let result = fruits.filter(function(v, i){
+//     console.log(v, i);
+//     return v == "Mango"
+// })
+
+// console.log(result);
+
+// let ages = [12,18,22,15,30];
+
+// let adults = ages.filter(function(age){
+//     return age >= 18;
+// });
+
+// console.log(adults);
+
+
+// let ages = [12,18,22,15,30];
+
+// let adults = ages.find(function(age){
+//     return age >= 18;
+// });
+
+// console.log(adults);
+
+
+// let marks = [20,30,70];
+
+// let result = marks.some(function(mark){
+//     return mark >= 50;
+// });
+
+// console.log(result);
+
+// let marks = [60,65,70];
+
+// let result = marks.every(function(mark){
+//     return mark >= 50;
+// });
+
+// console.log(result);
+
+// let result = fruits.includes("Banana")
+// console.log(result);
+
+
+// let arr = [1, 5, 7, 9, 2, 3, 4]
+let arr = [1, 15, 27, 3, 5, 19, 32, 43, 14, 100000]
+let alpha = ["a", "z", "c", "w", "u"]
+// let arrReverse = arr.sort().reverse()
+// arr.sort()
+// alpha.sort()
+// console.log(arr);
+// console.log(alpha);
+
+
+// fruits.reverse()
+
+// console.log(fruits);
+
+
+// let names = ["ahmed", "bilal", "owais", "jhon", "ali"]
+// names.sort()
+// console.log(names);
+
+
+// arr.sort(function(a,b){
+//     return a-b;
+// });
+
+// console.log(arr);
+
+
+// let sum = [10, 20, 90, 80]
+// let res = sum.reduce(function(a ,b){
+//     return a + b
+// })
+
+// console.log(res);
+
+
+// let cities = ["Karachi", "Pindi", "Lahore"]
+
+// let res = cities.join(">")
+// console.log(res);
+
+
+// let username = "hassan"
+// let res = username.split("")
+// console.log(res);
